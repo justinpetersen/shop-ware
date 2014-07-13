f56fc7313371053c8c8c10dd13087ef80a31ecbe
Justin Petersen <justin@justinpetersen.com>
2014-07-13 09:28:52 -0700

    Disabled sticky nav when browser is resized to mobile, removed horizontal padding from mobile nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -79,35 +79,68 @@ var ScrollWatcher = function() {
 };
 
 ScrollWatcher.prototype.onScroll = function() {
+	this.updateHeaderPosition();
+};
+
+ScrollWatcher.prototype.onResize = function() {
+	this.checkEnable();
+};
+
+ScrollWatcher.prototype.init = function(fade) {
+	this.fade = fade;
+	$(window).on('resize', $.proxy(this.onResize, this));
+	this.checkEnable();
+};
+
+ScrollWatcher.prototype.checkEnable = function() {
+	if ($(window).outerWidth() >= 768) {
+		this.enable();
+	} else {
+		this.disable();
+	}
+};
+
+ScrollWatcher.prototype.enable = function() {
+	$(window).on('scroll', $.proxy(this.onScroll, this));
+	this.updateHeaderPosition();
+};
+
+ScrollWatcher.prototype.disable = function() {
+	$(window).off('scroll');
+	this.hideFixedHeader();
+};
+
+ScrollWatcher.prototype.updateHeaderPosition = function() {
 	var offset = $('.header-bottom').offset().top;
 	if (this.headerOffset === -1) {
 		this.headerOffset = offset;
 	}
 	if ($(window).scrollTop() > this.headerOffset) {
-		$('.header-fixed').show();
-		if (this.fade)
-		{
-			$('.header-fixed').find('.logo').fadeIn();
-			$('.header-fixed').find('.sign-up-copy').fadeIn();
-			$('.header-fixed').find('.header-copy').fadeIn();
-		}
+		this.showFixedHeader();
 	} else {
-		$('.header-fixed').hide();
-		if (this.fade)
-		{
-			$('.header-fixed').find('.logo').hide();
-			$('.header-fixed').find('.sign-up-copy').hide();
-			$('.header-fixed').find('.header-copy').hide();
-		}
+		this.hideFixedHeader();
 	}
 };
 
-ScrollWatcher.prototype.init = function(fade) {
-	this.fade = fade;
-	$(window).on('scroll', $.proxy(this.onScroll, this));
+ScrollWatcher.prototype.showFixedHeader = function() {
+	$('.header-fixed').show();
+	if (this.fade)
+	{
+		$('.header-fixed').find('.logo').fadeIn();
+		$('.header-fixed').find('.sign-up-copy').fadeIn();
+		$('.header-fixed').find('.header-copy').fadeIn();
+	}
 };
 
-
+ScrollWatcher.prototype.hideFixedHeader = function() {
+	$('.header-fixed').hide();
+	if (this.fade)
+	{
+		$('.header-fixed').find('.logo').hide();
+		$('.header-fixed').find('.sign-up-copy').hide();
+		$('.header-fixed').find('.header-copy').hide();
+	}
+};
 
 /* INITIALIZE
 -------------------------------------------------- */
@@ -117,10 +150,8 @@ $(document).ready(function() {
 	var tabGroup = new TabGroup();
 	tabGroup.init();
 
-	if ($(window).outerWidth() >= 768) {
-		var fade = $('.header-fixed').find('.logo').length > 0;
-		var scrollWatcher = new ScrollWatcher();
-		scrollWatcher.init(fade);
-	}
+	var fade = $('.header-fixed').find('.logo').length > 0;
+	var scrollWatcher = new ScrollWatcher();
+	scrollWatcher.init(fade);
 
 });
