c4b8df308240634e8ef2263447f6be9200ab1398
Justin Petersen <justin@justinpetersen.com>
2014-07-23 17:03:10 -0700

    Moved WordPress theme into repo & styled News sidebar

---
+++ shop-ware-wp/footer.php
@@ -0,0 +1,43 @@
+
+
+
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid footer">
+            <div class=container>
+                <div class=row>
+                    <div class="col-xs-12 col-sm-6 col-md-6">
+                        <div class=footer-left-col>
+                            <p class=footer-heading>CONTACT US</p>
+                        </div>
+                        <div class=footer-middle-col>
+                            <p>EMAIL:<br><span class=footer-text><a href=mailto:support@shop-ware.com>SUPPORT@SHOP-WARE.COM</a></span></p>
+                            <p>ADDRESS:<br><span class=footer-text>475 9TH STREET<br>SAN FRANCISCO, CA 94103</span></p>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-6 footer-right-col">
+                        <p class=footer-text-legal>&copy; SHOP-WARE 2014 | <a href=#>PRIVACY POLICY</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class=gutter>&nbsp;</div>
+        </div>
+
+
+        <script src=/scripts/4776dee8.vendor.js></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+
+        <script src=/scripts/6b5da0e8.plugins.js></script>
+
+        <script src=/scripts/ea2a5853.news-main.js></script>
+
---
+++ shop-ware-wp/page.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+<div class="col-xs-12 col-sm-6 col-md-8 news-content">
+<div class="orange-bar hidden-xs">&nbsp;</div>
+<section id="content" role="main">
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<header class="header">
+<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+</header>
+<section class="entry-content">
+<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+<?php the_content(); ?>
+<div class="entry-links"><?php wp_link_pages(); ?></div>
+</section>
+</article>
+<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
+<?php endwhile; endif; ?>
+</section>
+</div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
