c41f60bb006d1b3b56c81f690f24f00191ad1ab0
Justin Petersen <justin@justinpetersen.com>
2014-07-27 16:45:41 -0700

    Enabled clicking on next arrow

--- app/scripts/tabgroup.js
+++ app/scripts/tabgroup.js
@@ -3,15 +3,26 @@
 /* TAB GROUP
 -------------------------------------------------- */
 
-var TabGroup = function() { };
+var TabGroup = function() {
+	this.activeTab = 0;
+};
 
 TabGroup.prototype.onTabClick = function(index, device) {
+	this.setActiveTab(index, device);
+};
+
+TabGroup.prototype.onNextClick = function(index, device) {
+	// KLUDGE: On desktop all the tab content is stacked on top of each other, so it will always appear like the 4th next arrow was clicked
+	if (device === 'desktop') {
+		index = (this.activeTab + 1) % 4;
+	}
 	this.setActiveTab(index, device);
 };
 
 TabGroup.prototype.init = function() {
 	this.initAccordion();
 	this.initTabs();
+	this.initNextArrows();
 };
 
 TabGroup.prototype.initTabs = function() {
@@ -24,7 +35,27 @@ TabGroup.prototype.initTabs = function() {
 	});
 };
 
+TabGroup.prototype.initNextArrows = function() {
+	var that = this;
+	$('.next-arrow.desktop').each(function(index) {
+		$(this).on('click', $.proxy(that.onNextClick, that, (index + 1) % 4, 'desktop'));
+	});
+	$('.next-arrow.mobile').each(function(index) {
+		$(this).on('click', $.proxy(that.onNextClick, that, (index + 1) % 4, 'mobile'));
+	});
+};
+
 TabGroup.prototype.setActiveTab = function(activeIndex, device) {
+	// Simulate clicking on the the tab
+	if (device === 'mobile') {
+		$('#collapse-' + this.activeTab).collapse('hide');
+		$('#collapse-' + activeIndex).collapse('show');
+	}
+
+	// Update the stored value for which tab is active
+	this.activeTab = activeIndex;
+
+	// Highlight the tab state
 	$('.home-tab.' + device).each(function(index) {
 		if (index === activeIndex) {
 			$(this).addClass('active');
@@ -35,6 +66,7 @@ TabGroup.prototype.setActiveTab = function(activeIndex, device) {
 		}
 	});
 
+	// Fade in the tab content
 	if (device === 'desktop') {
 		$('.home-tab-slide.desktop').each(function(index) {
 			if (index === activeIndex) {
@@ -78,6 +110,8 @@ TabGroup.prototype.cloneTabSlide = function(element, index) {
 	var clone = element.clone();
 	clone.addClass('active');
 	clone.removeClass('desktop');
+	clone.find('.next-arrow').removeClass('desktop');
+	clone.find('.next-arrow').addClass('mobile');
 	$('.panel-body').eq(index).append(clone);
 };
 
