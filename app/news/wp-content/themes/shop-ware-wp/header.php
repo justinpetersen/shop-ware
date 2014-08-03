6f5e14a269925163927b31df95977bad6241b2d7
Justin Petersen <justin@justinpetersen.com>
2014-08-03 16:03:40 -0700

    Organized images into folders for each page

--- app/news/wp-content/themes/shop-ware-wp/header.php
+++ app/news/wp-content/themes/shop-ware-wp/header.php
@@ -5,24 +5,18 @@
         <title>Shop-Ware</title>
         <meta name=description content="">
         <meta name=viewport content="width=device-width">
-        <link rel="shortcut icon" href=/ee07b75f.favicon.ico>
+        <link rel="shortcut icon" href=../ee07b75f.favicon.ico>
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-
-        <link rel=stylesheet href=/styles/0351c1da.main.css>
-        <script src=/scripts/vendor/fbe20327.modernizr.js></script>
+        <link rel=stylesheet href=../styles/9a8c7729.main.css>
+        <script src=../scripts/vendor/fbe20327.modernizr.js></script>
         <?php wp_head(); ?>
     
     <body>
-        <!--[if lt IE 10]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-
-
         <div class=container-fluid>
             <nav class="navbar navbar-default navbar-fixed-top" role=navigation>
                 <div class=container>
                     <div class=logo>
-                        <a href=/index.html><img src=/images/cac2b2e1.shop-ware-logo-sm.png width=115 height=53></a>
+                        <a href=../index.html><img src=../images/cac2b2e1.shop-ware-logo-sm.png alt=Shop-Ware width=115 height=53></a>
                     </div>
                     <!-- Brand and toggle get grouped for better mobile display -->
                     <div class=navbar-header>
@@ -36,16 +30,18 @@
                     <!-- Collect the nav links, forms, and other content for toggling -->
                     <div class="collapse navbar-collapse navbar-right" id=main-nav>
                         <ul class="nav navbar-nav">
-                            <li class=visible-xs><a href=/index.html>HOME</a></li>
-                            <li><a href=/pricing.html>PRICING</a></li>
-                            <li><a href=/about.html>ABOUT</a></li>
+                            <li class=visible-xs><a href=../index.html>HOME</a></li>
+                            <li><a href=../pricing.html>PRICING</a></li>
+                            <li><a href=../about.html>ABOUT</a></li>
                             <li class=active><a href="/news/">NEWS</a></li>
                             <li class=hidden-xs><div class=divider>|</div></li>
                             <li><a href=https://app.shop-ware.com/login>LOGIN</a></li>
                         </ul>
                     </div><!-- /.navbar-collapse -->
                     <div class=sign-up-copy>
-                        <a href=#><button type=button class="btn btn-primary sign-up-button"><h3>Get Started</h3></button></a>
+                        <a href=https://app.shop-ware.com/signup>
+                            <div class="btn btn-primary sign-up-button">Get Started</div>
+                        </a>
                     </div>
                 </div>
             </nav>
@@ -57,10 +53,10 @@
                 <div class=row>
                     <div class="col-xs-12 col-sm-6 col-md-8">
                         <div class="logo-container visible-xs">
-                            <a href=/index.html><img src=/images/cac2b2e1.shop-ware-logo-sm.png width=110 height=50></a>
+                            <a href=../index.html><img src=../images/cac2b2e1.shop-ware-logo-sm.png alt=Shop-Ware width=110 height=50></a>
                         </div>
                         <div class="logo-container hidden-xs">
-                            <a href=/index.html><img src=/images/cac2b2e1.shop-ware-logo-sm.png width=220 height=101></a>
+                            <a href=../index.html><img src=../images/cac2b2e1.shop-ware-logo-sm.png alt=Shop-Ware width=220 height=101></a>
                         </div>
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-4">
@@ -78,7 +74,9 @@
                     <div class="col-xs-12 col-sm-7 col-md-8">&nbsp;</div>
                     <div class="col-xs-12 col-sm-5 col-md-4">
                         <div class=sign-up-copy>
-                            <a href=#><button type=button class="btn btn-primary sign-up-button"><h3>Get Started</h3></button></a>
+                            <a href=https://app.shop-ware.com/signup>
+                                <div class="btn btn-primary sign-up-button">Get Started</div>
+                            </a>
                             <p class=sign-up-heading>Try it free for 30 days! No credit card required.</p>
                         </div>
                     </div>
@@ -88,6 +86,7 @@
         <div class="container-fluid news-main">
             <div class=container>
                 <div class="row row-eq-height">
+
 
 
 
